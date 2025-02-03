93ff46868171354e0779cedb452928478641acad
Darukity <samchgaming@gmail.com>
2025-02-03 11:53:39 +0100

    filter test case test fail

---
+++ exos/Authentification & Filtrage/Filter.js
@@ -0,0 +1,9 @@
+// 3. Exercices en JavaScript natif : Authentification & Filtrage
+
+function filter(array, key, searchValue) {
+    return
+} 
+
+module.exports = {
+    filter
+};
--- exos/Authentification & Filtrage/tests.js
+++ exos/Authentification & Filtrage/tests.js
@@ -44,3 +44,37 @@ let token = testGenerateToken(user);
 testVerifyToken(token);
 
 // 2. Filtrage
+// test for fillter
+const { filter } = require('./Filter.js');
+
+let userArray = [
+    { username: "le lutin", password: "malicieux", email: "lelutinmalicieux@malice.maléfique" },
+    { username: "le nain", password: "malicieux", email: "lenainmalicieux@malice.maléfique" },
+    { username: "le troll", password: "malicieux", email: "letrollmalcieux@malice.troll" },
+    { username: "le gobelin", password: "malicieux", email: "legobelinmalicieux@malice.lutin" }
+];
+
+let articleArray = [
+    { articleName: "Abeilles", articlePrice: 100, articleStock: 200 },
+    { articleName: "Cote de boeuf", articlePrice: 200, articleStock: 230 },
+    { articleName: "Pommes", articlePrice: 300, articleStock: 100 },
+    { articleName: "Cote de porc", articlePrice: 40, articleStock: 125 }
+];
+
+function testFilter(array, key, searchValue) {
+    let result = filter(array, key, searchValue);
+    try {
+        if (result.length > 0) {
+            console.log("Résultat du filtre: " + JSON.stringify(result));
+        } else {
+            throw new Error("Erreur lors du filtrage");
+        }
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+    return result;
+}
+
+testFilter(userArray, "username", "le lutin");
+testFilter(articleArray, "articleName", "Cote de boeuf");
