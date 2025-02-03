8d1ab9713aa686524266b646e648c77c415fb2ca
Darukity <samchgaming@gmail.com>
2025-02-03 11:56:31 +0100

    filter function test pass

--- exos/Authentification & Filtrage/Filter.js
+++ exos/Authentification & Filtrage/Filter.js
@@ -1,7 +1,7 @@
 // 3. Exercices en JavaScript natif : Authentification & Filtrage
 
 function filter(array, key, searchValue) {
-    return
+    return array.filter(item => item[key] === searchValue);
 } 
 
 module.exports = {
