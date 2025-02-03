d7736f11d560f47673f34ebf9c7c477dfff3b2dd
Darukity <samchgaming@gmail.com>
2025-02-03 17:34:45 +0100

    raw connexion to database and empty user entity

--- moviie_booker_api/src/app.module.ts
+++ moviie_booker_api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
+// Connects to the postgres database :
+// Source : https://medium.com/simform-engineering/nestjs-and-postgresql-a-crud-tutorial-32aa78778752
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'Worldpass@cces',
+      username: 'postgres',
+      entities: [],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
